d026a290e6549baedba5de67efec6ba0b0f183fd
Lang Hai <Lang@Lang-Hais-iMac.local>
2012-06-20 05:06:36 +0800

    stray error fix

--- src/server/scripts/EasternKingdoms/BlackwingDescent/boss_maloriak.cpp
+++ src/server/scripts/EasternKingdoms/BlackwingDescent/boss_maloriak.cpp
@@ -1,6 +1,5 @@
 ﻿/*
  * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
